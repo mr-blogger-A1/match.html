37c28a05ad85d8ee7a2ff20e05f9e0b7076a208f
mr-blogger-A1 <mohamedmmp@hotmail.com>
2025-08-20 01:28:10 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,197 @@
+[
+    {
+        "team1Name": "سيراميكا كليوباترا",
+        "team2Name": "إنبي",
+        "team1Logo": "https:\/\/tinyurl.com\/2bqtpqg2",
+        "team2Logo": "https:\/\/tinyurl.com\/2agjust3",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "06:00 م"
+    },
+    {
+        "team1Name": "وادي دجلة",
+        "team2Name": "بتروجيت",
+        "team1Logo": "https:\/\/tinyurl.com\/2a9oj8mb",
+        "team2Logo": "https:\/\/tinyurl.com\/25pze6fk",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "الجونة",
+        "team2Name": "غزل المحلة",
+        "team1Logo": "https:\/\/tinyurl.com\/22z7w6qc",
+        "team2Logo": "https:\/\/tinyurl.com\/2cqu3a9d",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "القادسية السعودي",
+        "team2Name": "الأهلي السعودي",
+        "team1Logo": "https:\/\/tinyurl.com\/257z8fa8",
+        "team2Logo": "https:\/\/tinyurl.com\/236maozk",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "03:00 م"
+    },
+    {
+        "team1Name": "بودو جليمت",
+        "team2Name": "شتورم جراتس",
+        "team1Logo": "https:\/\/tinyurl.com\/2bohw42d",
+        "team2Logo": "https:\/\/tinyurl.com\/277brysm",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "بازل",
+        "team2Name": "كوبنهاجن",
+        "team1Logo": "https:\/\/tinyurl.com\/2b28at56",
+        "team2Logo": "https:\/\/tinyurl.com\/2a3heuja",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "سيلتيك",
+        "team2Name": "قايرات ألماته",
+        "team1Logo": "https:\/\/tinyurl.com\/2yfssxjc",
+        "team2Logo": "https:\/\/tinyurl.com\/282lh6rm",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "فناربخشة",
+        "team2Name": "بنفيكا",
+        "team1Logo": "https:\/\/tinyurl.com\/263g5tcw",
+        "team2Logo": "https:\/\/tinyurl.com\/27cuo3rw",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "Redcar Athletic",
+        "team2Name": "Guisborough Town",
+        "team1Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team2Logo": "https:\/\/tinyurl.com\/285lz5c2",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:30 م"
+    },
+    {
+        "team1Name": "Hollands &amp; Blair",
+        "team2Name": "Knaphill",
+        "team1Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team2Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "غرايز أثليتيك",
+        "team2Name": "هيتكين تاون",
+        "team1Logo": "https:\/\/tinyurl.com\/2ae846fe",
+        "team2Logo": "https:\/\/tinyurl.com\/2cydbmqz",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "Ilford",
+        "team2Name": "واير",
+        "team1Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team2Logo": "https:\/\/tinyurl.com\/2yzb6dh5",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "Woodford Town",
+        "team2Name": "هارلو تاون",
+        "team1Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team2Logo": "https:\/\/tinyurl.com\/25w35m4x",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "Easington Colliery",
+        "team2Name": "Pickering Town",
+        "team1Logo": "https:\/\/tinyurl.com\/2a4vayd2",
+        "team2Logo": "https:\/\/tinyurl.com\/28xrd3xk",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "لم تبدأ",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "فيليس سارسفيلد",
+        "team2Name": "فورتاليزا",
+        "team1Logo": "https:\/\/tinyurl.com\/26khh6t8",
+        "team2Logo": "https:\/\/tinyurl.com\/2doxuj2f",
+        "team1Score": "1",
+        "team2Score": "0",
+        "Time-Start": "2025-08-20T03:30",
+      "Time-End": "2025-08-20T05:30",
+      "Time-Now": 90,
+        "status": "مباشر",
+        "matchTime": ""
+    },
+    {
+        "team1Name": "راسينغ كلوب",
+        "team2Name": "بينيارول",
+        "team1Logo": "https:\/\/tinyurl.com\/2bpnsvpo",
+        "team2Logo": "https:\/\/tinyurl.com\/2yhuyzem",
+        "team1Score": "",
+        "team2Score": "",
+        "Time-Start": "2025-08-20T03:30",
+      "Time-End": "2025-08-20T05:30",
+      "Time-Now": 90,
+      "Time-Zone": "+02:00",
+        "status": "لم تبدأ",
+        "matchTime": "03:30 ص"
+    },
+    {
+        "team1Name": "ساو باولو",
+        "team2Name": "ناسيونال ميديين",
+        "team1Logo": "https:\/\/tinyurl.com\/29mywnhv",
+        "team2Logo": "https:\/\/tinyurl.com\/264fxa22",
+        "team1Score": "",
+        "team2Score": "",
+        "Time-Start": "2025-08-20T03:30",
+      "Time-End": "2025-08-20T05:30",
+      "Time-Now": 90,
+      "Time-Zone": "+02:00",
+        "status": "لم تبدأ",
+        "matchTime": "03:30 ص"
+    },
+    {
+        "team1Name": "نيا سلامانيا",
+        "team2Name": "بايك",
+        "team1Logo": "https:\/\/tinyurl.com\/27sydkx2",
+        "team2Logo": "https:\/\/tinyurl.com\/2y9waryn",
+        "team1Score": "",
+        "team2Score": "",
+        "Time-Start": "2025-08-20T19:00",
+      "Time-End": "2025-08-20T21:00",
+      "Time-Now": 90,
+      "Time-Zone": "+02:00",
+        "status": "لم تبدأ",
+        "matchTime": "07:00 م"
+    }
+]

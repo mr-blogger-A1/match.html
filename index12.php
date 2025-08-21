ed2b3ea1b17e4d49e3df2d886ff67bfe5fac2d60
mr-blogger-A1 <mohamedmmp@hotmail.com>
2025-08-21 13:28:46 +0300

    Update and rename index11.php to index12.php

---
+++ index12.php
@@ -0,0 +1,462 @@
+[
+    {
+        "team1Name": "المقاولون العرب",
+        "team2Name": "حرس الحدود",
+        "team1Logo": "https://tinyurl.com/2ycz4s7y",
+        "team2Logo": "https://tinyurl.com/2cncansr",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:00 م"
+    },
+    {
+        "team1Name": "سموحة",
+        "team2Name": "زد",
+        "team1Logo": "https://tinyurl.com/2cu7mb2j",
+        "team2Logo": "https://tinyurl.com/2dhcjakc",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "مودرن سبورت",
+        "team2Name": "الزمالك",
+        "team1Logo": "https://tinyurl.com/25f287u4",
+        "team2Logo": "https://tinyurl.com/22am9a4e",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "ميتلاند",
+        "team2Name": "كوبيون بالوسورا",
+        "team1Logo": "https://tinyurl.com/29rrztpw",
+        "team2Logo": "https://tinyurl.com/28xcrv5k",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "07:30 م"
+    },
+    {
+        "team1Name": "مالمو",
+        "team2Name": "سيجما أولوموتس",
+        "team1Logo": "https://tinyurl.com/2a6dhsr2",
+        "team2Logo": "https://tinyurl.com/2747s3yl",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "بران",
+        "team2Name": "آيك لارناكا",
+        "team1Logo": "https://tinyurl.com/22ob5hjp",
+        "team2Logo": "https://tinyurl.com/29xkg2qu",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "تيتوفو",
+        "team2Name": "لودوجوريتس رازجراد",
+        "team1Logo": "https://tinyurl.com/224f4wyd",
+        "team2Logo": "https://tinyurl.com/2bwdzs3u",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "باناثينايكوس",
+        "team2Name": "سامسون سبور",
+        "team1Logo": "https://tinyurl.com/22mfan6p",
+        "team2Logo": "https://tinyurl.com/2bgw97df",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "موستار",
+        "team2Name": "أوتريخت",
+        "team1Logo": "https://tinyurl.com/24cjxbur",
+        "team2Logo": "https://tinyurl.com/28tp5e4z",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "سلوفان براتيسلافا",
+        "team2Name": "يونج بويز",
+        "team1Logo": "https://tinyurl.com/25c56xlm",
+        "team2Logo": "https://tinyurl.com/24vgyjck",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:15 م"
+    },
+    {
+        "team1Name": "ليخ بوزنان",
+        "team2Name": "جينك",
+        "team1Logo": "https://tinyurl.com/27mt83ao",
+        "team2Logo": "https://tinyurl.com/2b8bh6vj",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:30 م"
+    },
+    {
+        "team1Name": "رييكا",
+        "team2Name": "باوك سالونيكي",
+        "team1Logo": "https://tinyurl.com/2xnlc267",
+        "team2Logo": "https://tinyurl.com/23ddobfo",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "أبردين",
+        "team2Name": "ستياوا بوخارست",
+        "team1Logo": "https://tinyurl.com/2cjglrnh",
+        "team2Logo": "https://tinyurl.com/2cvgplr3",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "لينكولن ريد أمبس",
+        "team2Name": "سبورتينغ براجا",
+        "team1Logo": "https://tinyurl.com/2yn8o3dv",
+        "team2Logo": "https://tinyurl.com/2bo76tz8",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "روزنبورج",
+        "team2Name": "ماينز",
+        "team1Logo": "https://tinyurl.com/27k3b7xn",
+        "team2Logo": "https://tinyurl.com/2xr94b8c",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "07:00 م"
+    },
+    {
+        "team1Name": "ايه سي فولفسبرجر",
+        "team2Name": "أومونيا نيقوسيا",
+        "team1Logo": "https://tinyurl.com/2c6qtk73",
+        "team2Logo": "https://tinyurl.com/26tykqba",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "حمرون سبارتانس",
+        "team2Name": "داوجافا ريجا",
+        "team1Logo": "https://tinyurl.com/29fbt3aq",
+        "team2Logo": "https://tinyurl.com/24oacdua",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "هاكين",
+        "team2Name": "1907 كلوج",
+        "team1Logo": "https://tinyurl.com/299br69x",
+        "team2Logo": "https://tinyurl.com/24wyxo9y",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "غيوري",
+        "team2Name": "رابيد فيينا",
+        "team1Logo": "https://tinyurl.com\/29ye3ety",
+        "team2Logo": "https://tinyurl.com\/24uvlk2r",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:00 م"
+    },
+    {
+        "team1Name": "إسطنبول باشاك شهير",
+        "team2Name": "كرايوفا",
+        "team1Logo": "https:\/\/tinyurl.com\/25886xtf",
+        "team2Logo": "https:\/\/tinyurl.com\/2bst4u9o",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:45 م"
+    },
+    {
+        "team1Name": "ستراسبورج",
+        "team2Name": "بروندبي",
+        "team1Logo": "https:\/\/tinyurl.com\/2at9w8zt",
+        "team2Logo": "https:\/\/tinyurl.com\/253hzm24",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "سبارتا براغ",
+        "team2Name": "ريجا",
+        "team1Logo": "https:\/\/tinyurl.com\/2bdxnegq",
+        "team2Logo": "https:\/\/tinyurl.com\/23jqgwat",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "تسيليى",
+        "team2Name": "بانيك أوسترافا",
+        "team1Logo": "https:\/\/tinyurl.com\/2y2kwunr",
+        "team2Logo": "https:\/\/tinyurl.com\/27prrujx",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "بريدابليك",
+        "team2Name": "فيرتوس أكوافيفا",
+        "team1Logo": "https:\/\/tinyurl.com\/29yazobu",
+        "team2Logo": "https:\/\/tinyurl.com\/2dnlgxff",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "دريتا",
+        "team2Name": "ديفيردانج 03",
+        "team1Logo": "https:\/\/tinyurl.com\/2bfu6mh2",
+        "team2Logo": "https:\/\/tinyurl.com\/2cuulvvc",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "نيمان غروندو",
+        "team2Name": "رايو فاليكانو",
+        "team1Logo": "https:\/\/tinyurl.com\/253s5qkg",
+        "team2Logo": "https:\/\/tinyurl.com\/28b5kq9o",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "أندرلخت",
+        "team2Name": "أيك أثينا",
+        "team1Logo": "https:\/\/tinyurl.com\/25fs9ko2",
+        "team2Logo": "https:\/\/tinyurl.com\/26fxaece",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "أولمبيا",
+        "team2Name": "نواه",
+        "team1Logo": "https:\/\/tinyurl.com\/23e4vcng",
+        "team2Logo": "https:\/\/tinyurl.com\/2486j3uy",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "شاختار دونيتسك",
+        "team2Name": "سيرفيت جنيف",
+        "team1Logo": "https:\/\/tinyurl.com\/288zf9lo",
+        "team2Logo": "https:\/\/tinyurl.com\/2ah5yap3",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "Zhytomyr",
+        "team2Name": "فيورنتينا",
+        "team1Logo": "https:\/\/tinyurl.com\/27udl5n4",
+        "team2Logo": "https:\/\/tinyurl.com\/23d7g3gd",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "ليفسكي صوفيا",
+        "team2Name": "آي زي ألكمار",
+        "team1Logo": "https:\/\/tinyurl.com\/23s7kt4c",
+        "team2Logo": "https:\/\/tinyurl.com\/2cq5p6uv",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:00 م"
+    },
+    {
+        "team1Name": "ياغييلونيا بياويستوك",
+        "team2Name": "دينامو تيرانا",
+        "team1Logo": "https:\/\/tinyurl.com\/24alugrr",
+        "team2Logo": "https:\/\/tinyurl.com\/26udaltq",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:15 م"
+    },
+    {
+        "team1Name": "لوزان سبورت",
+        "team2Name": "بشكتاش",
+        "team1Logo": "https:\/\/tinyurl.com\/22xbp2eh",
+        "team2Logo": "https:\/\/tinyurl.com\/28nzk29n",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:15 م"
+    },
+    {
+        "team1Name": "شيلبورن",
+        "team2Name": "لينفيلد",
+        "team1Logo": "https:\/\/tinyurl.com\/29ygbp72",
+        "team2Logo": "https:\/\/tinyurl.com\/283kpgq5",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "09:45 م"
+    },
+    {
+        "team1Name": "سانتا كلارا",
+        "team2Name": "شامروك روفرز",
+        "team1Logo": "https:\/\/tinyurl.com\/2d2cugzc",
+        "team2Logo": "https:\/\/tinyurl.com\/2bvve655",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "راكوف تشانستوخوفا",
+        "team2Name": "أردا كارادزالي",
+        "team1Logo": "https:\/\/tinyurl.com\/2yx4c4jv",
+        "team2Logo": "https:\/\/tinyurl.com\/29hbq8xx",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "كريستال بالاس",
+        "team2Name": "فريدريكشتاد",
+        "team1Logo": "https:\/\/tinyurl.com\/284u72xk",
+        "team2Logo": "https:\/\/tinyurl.com\/22vedotm",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "هيبرنيان",
+        "team2Name": "ليغيا وارسو",
+        "team1Logo": "https:\/\/tinyurl.com\/24wabfla",
+        "team2Logo": "https:\/\/tinyurl.com\/27jyokn9",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "10:00 م"
+    },
+    {
+        "team1Name": "إستوديانتس",
+        "team2Name": "سيرو بورتينيو",
+        "team1Logo": "https:\/\/tinyurl.com\/2beyxkl4",
+        "team2Logo": "https:\/\/tinyurl.com\/25ndb9gq",
+        "team1Score": "0",
+        "team2Score": "0",
+        "status": "انتهت",
+        "matchTime": "01:00 ص"
+    },
+    {
+        "team1Name": "إنترناسيونال",
+        "team2Name": "فلامينجو",
+        "team1Logo": "https:\/\/tinyurl.com\/25lp9prn",
+        "team2Logo": "https:\/\/tinyurl.com\/26ykna38",
+        "team1Score": "0",
+        "team2Score": "2",
+        "status": "انتهت",
+        "matchTime": "03:30 ص"
+    },
+    {
+        "team1Name": "نادي قطر",
+        "team2Name": "السيلية",
+        "team1Logo": "https:\/\/tinyurl.com\/23nf85hw",
+        "team2Logo": "https:\/\/tinyurl.com\/2bud4s4d",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:30 م"
+    },
+    {
+        "team1Name": "الأهلي القطري",
+        "team2Name": "السد",
+        "team1Logo": "https:\/\/tinyurl.com\/2afcmzn3",
+        "team2Logo": "https:\/\/tinyurl.com\/2xo9jb9v",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "08:30 م"
+    },
+    {
+        "team1Name": "المستقبل الرياضي بسليمان",
+        "team2Name": "الملعب التونسي",
+        "team1Logo": "https:\/\/tinyurl.com\/2yor3f3u",
+        "team2Logo": "https:\/\/tinyurl.com\/245bc92s",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:30 م"
+    },
+    {
+        "team1Name": "الشبيبة الرياضية بالعمران",
+        "team2Name": "البنزرتي",
+        "team1Logo": "https:\/\/tinyurl.com\/2avwd2hu",
+        "team2Logo": "https:\/\/tinyurl.com\/2ynooaun",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:30 م"
+    },
+    {
+        "team1Name": "اتحاد بن قردان",
+        "team2Name": "النجم الساحلي",
+        "team1Logo": "https:\/\/tinyurl.com\/2d2x2ylw",
+        "team2Logo": "https:\/\/tinyurl.com\/24ulyfhf",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:30 م"
+    },
+    {
+        "team1Name": "شبيبة القيروان",
+        "team2Name": "الترجي التونسي",
+        "team1Logo": "https:\/\/tinyurl.com\/23vysqqk",
+        "team2Logo": "https:\/\/tinyurl.com\/294hm3uz",
+        "team1Score": "",
+        "team2Score": "",
+        "status": "قادمة",
+        "matchTime": "06:30 م"
+    }
+]
